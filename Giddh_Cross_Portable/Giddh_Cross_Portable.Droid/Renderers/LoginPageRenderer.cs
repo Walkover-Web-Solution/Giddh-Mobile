0282daba9797f26c4d64f569a8dfe5b6074cd088
Ravi soni <ravisoni@hostnsoft.com>
2016-04-02 21:08:07 +0530

    after android store upload

--- Giddh_Cross_Portable/Giddh_Cross_Portable.Droid/Renderers/LoginPageRenderer.cs
+++ Giddh_Cross_Portable/Giddh_Cross_Portable.Droid/Renderers/LoginPageRenderer.cs
@@ -20,34 +20,18 @@ using Xamarin.Forms.Platform.Android;
 namespace Giddh_Cross_Portable.Droid.Renderers
 {
     class LoginPageRenderer : PageRenderer
-    {        
+    {     
         protected override void OnElementChanged(ElementChangedEventArgs<Page> e)
         {
             base.OnElementChanged(e);
 
             // this is a ViewGroup - so should be able to load an AXML file and FindView<>
-            var activity = this.Context as Activity;
-
-            //var auth = new OAuth2Authenticator(
-            //    clientId: App.Instance.OAuthSettings.ClientId, // your OAuth2 client id
-            //    clientSecret:App.Instance.OAuthSettings.ClientSecret,
-            //    scope: App.Instance.OAuthSettings.Scope, // The scopes for the particular API you're accessing. The format for this will vary by API.
-            //    authorizeUrl: new Uri(App.Instance.OAuthSettings.AuthorizeUrl), // the auth URL for the service
-            //    redirectUrl: new Uri(App.Instance.OAuthSettings.RedirectUrl),
-            //    accessTokenUrl: new Uri("https://accounts.google.com/o/oauth2/token"),
-            //    getUsernameAsync: null); // the redirect URL for the service
-            //auth.ShowUIErrors = false;
-            //auth.Title = "Giddh Manager";
-            //auth.Completed += Auth_Completed;
-            //try
-            //{                
-            //    activity.StartActivity(auth.GetUI(activity));
-            //}
-            //catch (Exception ex)
-            //{
-
-            //}
-            LoginByGoogle(true);
+            try
+            {
+                LoginByGoogle(false);
+            }
+            catch (AggregateException ex)
+            { }
         }
 
         private async void Auth_Completed(object sender, AuthenticatorCompletedEventArgs e)
@@ -59,7 +43,7 @@ namespace Giddh_Cross_Portable.Droid.Renderers
             }
             string access_token;
             e.Account.Properties.TryGetValue("access_token", out access_token);
-            App.Instance.SuccessfulLoginAction.Invoke();
+            //App.Instance.SuccessfulLoginAction.Invoke();
             // Use eventArgs.Account to do wonderful things
             Console.WriteLine(access_token);
             App.Instance.SaveToken(access_token);
